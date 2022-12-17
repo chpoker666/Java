22e30bfce96780dc368552d456a112cdf9d20554
chpoker666 <115729065+chpoker666@users.noreply.github.com>
2022-12-17 20:34:18 +0300

    Add files via upload

---
+++ pr2/src/piskun/TestAuthor.java
@@ -0,0 +1,8 @@
+package piskun;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Иван", "ivan@ivan.ru", 'm');
+        System.out.println(a);
+    }
+}
